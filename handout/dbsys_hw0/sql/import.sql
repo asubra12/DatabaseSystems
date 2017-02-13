1f085dc967611f119bb7a0018fcacb188eed68ab
Anshul <asubra12@jhu.edu>
2017-02-12 21:39:34 -0500

    Few Changes

--- handout/dbsys_hw0/sql/import.sql
+++ handout/dbsys_hw0/sql/import.sql
@@ -4,11 +4,11 @@
 -- Notes:
 --   1) The csv file for <table> is located at '~cs416/datasets/hw0/tpch-sf0.1/<table>.csv'.
 
-.import /home/cs416/datasets/hw0/tpch-sf0.1/part.csv part
-.import /home/cs416/datasets/hw0/tpch-sf0.1/supplier.csv supplier
-.import /home/cs416/datasets/hw0/tpch-sf0.1/partsupp.csv partsupp
-.import /home/cs416/datasets/hw0/tpch-sf0.1/customer.csv customer
-.import /home/cs416/datasets/hw0/tpch-sf0.1/orders.csv orders
-.import /home/cs416/datasets/hw0/tpch-sf0.1/lineitem.csv lineitem
-.import /home/cs416/datasets/hw0/tpch-sf0.1/nation.csv nation
-.import /home/cs416/datasets/hw0/tpch-sf0.1/region.csv region
+.import ~/Documents/Junior\ Year/Database\ Systems/HW0/tpch-sf0.1/part.csv part
+.import ~/Documents/Junior\ Year/Database\ Systems/HW0/tpch-sf0.1/supplier.csv supplier
+.import ~/Documents/Junior\ Year/Database\ Systems/HW0/tpch-sf0.1/partsupp.csv partsupp
+.import ~/Documents/Junior\ Year/Database\ Systems/HW0/tpch-sf0.1/customer.csv customer
+.import ~/Documents/Junior\ Year/Database\ Systems/HW0/tpch-sf0.1/orders.csv orders
+.import ~/Documents/Junior\ Year/Database\ Systems/HW0/tpch-sf0.1/lineitem.csv lineitem
+.import ~/Documents/Junior\ Year/Database\ Systems/HW0/tpch-sf0.1/nation.csv nation
+.import ~/Documents/Junior\ Year/Database\ Systems/HW0/tpch-sf0.1/region.csv region
