91bef88840269ccc607569219e6f8ccf2175e937
Anshul <asubra12@jhu.edu>
2017-02-13 03:55:27 -0500

    Completed q4

--- handout/dbsys_hw0/sql/q4.todo.sql
+++ handout/dbsys_hw0/sql/q4.todo.sql
@@ -9,3 +9,35 @@
 -- Student SQL code here:
 
 
+DROP TABLE IF EXISTS cust_supp;
+CREATE TABLE cust_supp (
+	cust INTEGER,
+	supp INTEGER,
+	val REAL
+);
+
+INSERT INTO cust_supp (cust, supp, val)
+	select n1.n_nationkey as cust, n2.n_nationkey as supp, sum(o.o_totalprice) as val
+	FROM nation AS n1
+		JOIN customer AS c on c.c_nationkey = n1.n_nationkey
+		JOIN orders AS o on o.o_custkey = c.c_custkey
+		JOIN lineitem AS l on l.l_orderkey = o.o_orderkey
+		JOIN supplier AS s on s.s_suppkey = l.l_suppkey
+		JOIN nation as n2 on n2.n_nationkey = s.s_nationkey
+	GROUP BY cust, supp;
+
+WITH top_five AS (
+	select cust 
+	from cust_supp
+	group by cust
+	order by sum(val) DESC
+	limit 5),
+	supp_order AS (
+	select * from cust_supp
+	order by cust asc, val desc)
+select top_five.cust, supp_order.supp, supp_order.val
+from top_five join supp_order on top_five.cust = supp_order.cust
+where supp_order.supp IN (
+	select s2.supp from supp_order s2
+	where s2.cust = top_five.cust
+	limit 5);
