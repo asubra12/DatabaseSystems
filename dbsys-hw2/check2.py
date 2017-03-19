f1178eda0ba42100a1e6db52331e54ad57afc755
echiang3 <eric.chiang.24@gmail.com>
2017-03-19 14:15:31 -0400

    new queries

--- dbsys-hw2/check2.py
+++ dbsys-hw2/check2.py
@@ -24,7 +24,8 @@ query1 = db.query().fromTable('part')\
 
 # Query 1 - Hash Join
 query1 = db.query().fromTable('part')\
-    .join(db.query().fromTable('partsupp').where('PS_AVAILQTY == 1'),
+    .join(db.query()\
+          .fromTable('partsupp').where('PS_AVAILQTY == 1'),
           rhsSchema = db.relationSchema('partsupp'),
           method = 'hash',
           lhsHashFn = 'hash(P_PARTKEY) % 111', lhsKeySchema = DBSchema('P_PARTKEY', [('P_PARTKEY', 'int')]),
@@ -34,8 +35,7 @@ query1 = db.query().fromTable('part')\
           method = 'hash',
           lhsHashFn = 'hash(PS_SUPPKEY) % 111', lhsKeySchema = DBSchema('PS_SUPPKEY', [('PS_SUPPKEY', 'int')]),
           rhsHashFn = 'hash(S_SUPPKEY) % 111', rhsKeySchema = DBSchema('S_SUPPKEY', [('S_SUPPKEY', 'int')]))\
-    .union(
-    db.query().fromTable('part')
+    .union(db.query().fromTable('part')\
         .join(db.query().fromTable('partsupp').where('PS_SUPPLYCOST < 5'),
               rhsSchema = db.relationSchema('partsupp'),
               method = 'hash',
@@ -45,7 +45,7 @@ query1 = db.query().fromTable('part')\
               rhsSchema = db.relationSchema('supplier'),
               method = 'hash',
               lhsHashFn = 'hash(PS_SUPPKEY) % 111', lhsKeySchema = DBSchema('PS_SUPPKEY', [('PS_SUPPKEY', 'int')]),
-              rhsHashFn = 'hash(S_SUPPKEY) % 111', rhsKeySchema = DBSchema('S_SUPPKEY', [('S_SUPPKEY', 'int')])))\
+              rhsHashFn = 'hash(S_SUPPKEY) % 111', rhsKeySchema = DBSchema('S_SUPPKEY', [('S_SUPPKEY', 'int')]))\
     .select({'P_NAME': ('P_NAME', 'char(55)'), 'S_NAME': ('S_NAME', 'char(25)')})\
     .finalize()
 
@@ -70,9 +70,40 @@ query1 = db.query().fromTable('part')\
           lhsHashFn = 'hash(P_PARTKEY) % 111', lhsKeySchema = DBSchema('P_PARTKEY', [('P_PARTKEY', 'int')]),
           rhsHashFn = 'hash(L_PARTKEY) % 111', rhsKeySchema = DBSchema('L_PARTKEY', [('L_PARTKEY', 'int')]))\
           expr = 'P_PARTKEY == L_PARTKEY')\
-    .groupBy(groupSchema = DBSchem('P_NAME', [('P_NAME', 'char(55)')],
+    .groupBy(groupSchema = DBSchema('P_NAME', [('P_NAME', 'char(55)')],
                                    aggSchema = DBSchema('COUNT', [('COUNT', 'int')]),
                                    groupExpr(lambda e: e.P_NAME),
                                    aggExprs = [(0, lambda acc, e:acc + 1, lambda x: x)],
                                    groupHashFn = (lambda gbVal: hash(gbVal[0]) % 111)))\
+    .finalize()
+
+# Query 3 - Block-Nested-Loops Join
+query1 = db.query().fromTable('nation')\
+    .join(db.query().fromTable('customer'),
+          rhsSchema = db.relationSchema('customer'),
+          method = 'block-nested-loops',
+          expr = 'N_NATIONKEY == C_NATIONKEY')\
+    .join(db.query().fromTable('orders'),
+          rhsSchema = db.relationSchema('orders'),
+          method = 'block-nested-loops',
+          expr = 'C_CUSTKEY == O_CUSTKEY')\
+    .join(db.query().fromTable('lineitem'),
+          rhsSchema=db.relationSchema('lineitem'),
+          method='block-nested-loops',
+          expr='O_ORDERKEY == L_ORDERKEY')\
+    .join(db.query().fromTable('part'),
+          rhsSchema=db.relationSchema('part'),
+          method='block-nested-loops',
+          expr='L_PARTKEY == P_PARTKEY')\
+    .groupBy(groupSchema = DBSchema('FIRST', [('N_NAME', 'char(25)'), ('P_NAME', 'char(55)')]),
+             aggSchema = DBSchema('SUM', [('SUM', 'int')]),
+             groupExpr = (lambda e: (e.N_NAME, e.P_NAME)),
+             aggExprs = [(0, lambda acc, e: acc + e.L_QUANTITY, lambda x: x)],
+             groupHashFn = (lambda gbVal: hash(gbVal[0]) % 111))\
+    .groupBy(groupSchema = DBSchema('SECOND', [('N_NAME')]),
+             aggSchema = DBSchema('MAX', [('MAX', 'int')]),
+             groupExpr = (lambda e: e.N_NAME),
+             aggExprs = [(0, lambda acc, e: max(acc, e.SUM), lambda x: x)],
+             groupHashFn = (lambda gbVal: hash(gbVal[0]) % 111))\
+    .select({'NATION': ('N_NAME', 'char(25)'), 'MAX': ('MAX', 'int')})\
     .finalize()
