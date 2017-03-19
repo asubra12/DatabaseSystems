5c00fc5a60eaa1376707ea8a952e5d7f3d879f26
Anshul Subramanya 600.316 Sp17 <asubra12@ugrad4.cs.jhu.edu>
2017-03-19 03:11:57 -0400

    new experiments file

--- dbsys-hw2/experiments.py
+++ dbsys-hw2/experiments.py
@@ -9,7 +9,7 @@ import unittest
 
 import warnings
 
-def sqlite_tests(db):
+def sqlite_tests(db, printOutput):
 
     query1 = ''' select p.p_name, s.s_name
     from part p, supplier s, partsupp ps
@@ -61,19 +61,24 @@ def sqlite_tests(db):
     queries = [query1, query2, query3a, query3b]
 
     results = {}
+    
+    names = ['query1', 'query2', 'query3a', 'query3b']
 
     results[db] = []
     conn = sqlite3.connect(db)
     c = conn.cursor()
 
-    for query in queries:
+    for i in range(len(queries)):
         start = time.time()
-        c.execute(query)
+        processedQuery = c.execute(query[i])
         end = time.time()
         duration = end - start
-        results[db].append(duration)
-
-    return results
+        print('Database: ', db)
+        print('Query: ', names[i])
+        if printOutput:
+	    for row in processedQuery:
+		print(row)
+	print('Time', duration)	
 
 def setup():
     db = Database.Database()
@@ -104,58 +109,17 @@ def query1BNL_test(db):
         .finalize()
 
     start = time.time()
-    processedQuery=db.getResults(query1)
+    processedQuery=[query1.schema().unpack(tup) for page in 
+db.processQuery(query1) for tup in page[1]]
     end = time.time()
-    duration = start-end
+    duration = end-start
+    print('Query: Query 1, Block Nested Loops')
     print('Results: ', processedQuery)
     print('Time', duration)
     return
 
 
-#
-# def query3a_test(db, joinMethod='hash'):
-#     query = db.query().fromTable('nation').join( \
-#         db.query.fromTable('customer'),
-#         rhsSchema=db.relationSchema('customer'),
-#         method=joinMethod,
-#         lhsKeySchema=DBSchema('N_NATIONKEY', [('N_NATIONKEY', 'int')]),
-#         lhsHashFn='hash(N_NATIONKEY) % 111',
-#
-#
 
-    # )
-
-
-    '''
-    query3a = with temp as (
-       select n.n_name as nation, p.p_name as part, sum(l.l_quantity) as num
-       from customer c, nation n, orders o, lineitem l, part p
-       where c.c_nationkey = n.n_nationkey
-         and c.c_custkey = o.o_custkey
-         and o.o_orderkey = l.l_orderkey
-         and l.l_partkey = p.p_partkey
-       group by n.n_name, p.p_name
-    )
-    select nation, max(num)
-    from temp
-    group by nation;
-    '''
-
-    '''
-    schema = self.db.relationSchema('employee')
-    e2schema   = schema.rename('employee2', {'id':'id2', 'age':'age2', 'dept_id': 'dept_id2'})
-    keySchema  = DBSchema('employeeKey',  [('id', 'int')])
-    keySchema2 = DBSchema('employeeKey2', [('id2', 'int')])
-    hashJoin = self.db.query().fromTable('employee').join( \
-                 self.db.query().fromTable('employee'), \
-                 rhsSchema=e2schema, \
-                 method='hash', \
-                 lhsHashFn='hash(id) % 4',  lhsKeySchema=keySchema, \
-                 rhsHashFn='hash(id2) % 4', rhsKeySchema=keySchema2, \
-               ).finalize()
-    results = self.getResults(hashJoin)
-    self.assertEqual(len(results), self.numEmployees)
-    '''
 # databases = ['HW2_0.001.db', 'HW2_0.01.db']
 # print(sqlite_tests(databases[0]))
 # print(sqlite_tests(databases[1]))
