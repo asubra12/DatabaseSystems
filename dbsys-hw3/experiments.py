022321f6b2c6158d013beb1a2606b2cd35d71617
echiang3 <eric.chiang.24@gmail.com>
2017-04-24 01:26:41 -0400

    experiments are done I think

--- dbsys-hw3/experiments.py
+++ dbsys-hw3/experiments.py
@@ -66,7 +66,92 @@ def query3(db):
         .finalize()
     return query
 
+def query4(db):
+    query = db.query().fromTable('nation')\
+        .join(
+            db.query().fromTable('customer'),
+            method='hash',
+            lhsHashFn='hash(N_NATIONKEY) % 11', lhsKeySchema=DBSchema('nationKey1', [('N_NATIONKEY', 'int')]),
+            rhsHashFn='hash(C_NATIONKEY) % 11', rhsKeySchema=DBSchema('nationKey2', [('N_NATIONKEY', 'int')]))\
+        .join(
+            db.query().fromTable('orders'),
+            method='hash',
+            lhsHashFn='hash(C_CUSTKEY) % 11', lhsKeySchema=DBSchema('custKey1', [('C_CUSTKEY', 'int')]),
+            rhsHashFn='hash(O_CUSTKEY) % 11', rhsKeySchema=DBSchema('custKey2', [('O_CUSTKEY', 'int')])) \
+        .join(
+            db.query().fromTable('lineitem'),
+            method='hash',
+            lhsHashFn='hash(O_ORDERKEY) % 11', lhsKeySchema=DBSchema('orderKey1', [('O_ORDERKEY', 'int')]),
+            rhsHashFn='hash(L_ORDERKEY) % 11', rhsKeySchema=DBSchema('orderKey2', [('L_ORDERKEY', 'int')])) \
+        .where(
+            "O_ORDERDATE >= 19931001 and O_ORDERDATE < 19940101 and L_RETURNFLAG = 'R'")\
+        .groupBy(
+            groupSchema=DBSchema('groupKey', [('C_CUSTKEY','int'),('C_NAME','char(25)'),('C_ACCTBAL','float'),('C_PHONE','char(15)'),('N_NAME','char(25)'),('C_ADDRESS','char(40)'),('C_COMMENT','char(117)')]),
+            aggSchema=DBSchema('groupAgg', [('revenue', 'float')]),
+            groupExpr=(lambda e: (e.C_CUSTKEY, e.C_NAME, e.C_ACCTBAL, e.C_PHONE, e.N_NAME, e.N_NAME, e.C_ADDRESS, e.C_COMMENT)),
+            aggExprs=[(0, lambda acc, e: acc + (e.L_EXTENDEDPRICE * (1 - e.L_DISCOUNT)), lambda x: x)],
+            groupHashFn=(lambda gbVal: hash(gbVal) % 11))\
+        .select(
+            {'c_custkey': ('C_CUSTKEY', 'int'),
+             'c_name': ('C_NAME', 'char(25)'),
+             'revenue': ('revenue', 'float'),
+             'c_acctbal': ('C_ACCTBAL', 'float'),
+             'n_name': ('N_NAME', 'char(25)'),
+             'c_address': ('C_ADDRESS', 'char(40)'),
+             'c_phone': ('C_PHONE', 'char(15)'),
+             'c_comment': ('C_COMMENT', 'char(117)')})\
+        .finalize()
+    return query
+
+def query5(db):
+    query = db.query().fromTable('region')\
+        .join(
+            db.query().fromTable('nation'),
+            method='hash',
+            lhsHashFn='hash(R_REGIONKEY) % 11', lhsKeySchema=DBSchema('regionKey1', [('R_REGIONKEY', 'int')]),
+            rhsHashFn='hash(N_REGIONKEY) % 11', rhsKeySchema=DBSchema('regionKey2', [('N_REGIONKEY', 'int')]))\
+        .join(
+            db.query().fromTable('supplier'),
+            method='hash',
+            lhsHashFn='hash(N_NATIONKEY) % 11', lhsKeySchema=DBSchema('nationKey1', [('N_NATIONKEY', 'int')]),
+            rhsHashFn='hash(S_NATIONKEY) % 11', rhsKeySchema=DBSchema('nationKey2', [('S_NATIONKEY', 'int')])) \
+        .join(
+            db.query().fromTable('lineitem'),
+            method='hash',
+            lhsHashFn='hash(S_SUPPKEY) % 11', lhsKeySchema=DBSchema('suppKey1', [('S_SUPPKEY', 'int')]),
+            rhsHashFn='hash(L_SUPPKEY) % 11', rhsKeySchema=DBSchema('suppKey2', [('L_SUPPKEY', 'int')])) \
+        .join(
+            db.query().fromTable('orders'),
+            method='hash',
+            lhsHashFn='hash(L_ORDERKEY) % 11', lhsKeySchema=DBSchema('orderKey1', [('L_ORDERKEY', 'int')]),
+            rhsHashFn='hash(O_ORDERKEY) % 11', rhsKeySchema=DBSchema('orderKey2', [('O_ORDERKEY', 'int')])) \
+        .join(
+            db.query().fromTable('customer'),
+            method='hash',
+            lhsHashFn='hash(O_CUSTKEY) % 11', lhsKeySchema=DBSchema('custKey1', [('O_CUSTKEY', 'int')]),
+            rhsHashFn='hash(C_CUSTKEY) % 11', rhsKeySchema=DBSchema('custKey2', [('C_CUSTKEY', 'int')])) \
+        .join(
+            db.query().fromTable('supplier'),
+            method='hash',
+            lhsHashFn='hash(C_NATIONKEY) % 11', lhsKeySchema=DBSchema('nationKey3', [('C_NATIONKEY', 'int')]),
+            rhsHashFn='hash(S_NATIONKEY) % 11', rhsKeySchema=DBSchema('nationKey4', [('S_NATIONKEY', 'int')])) \
+        .where(
+            "R_NAME == 'ASIA' and O_ORDERDATE >= 19940101 and O_ORDERDATE < 19950101")\
+        .groupBy(
+            groupSchema=DBSchema('groupKey', [('N_NAME','char(25)')]),
+            aggSchema=DBSchema('groupAgg', [('revenue', 'float')]),
+            groupExpr=(lambda e: e.N_NAME),
+            aggExprs=[(0, lambda acc, e: acc + (e.L_EXTENDEDPRICE * (1 - e.L_DISCOUNT)), lambda x: x)],
+            groupHashFn=(lambda gbVal: hash(gbVal) % 11))\
+        .select(
+            {'n_name': ('n_name', 'char(25)'),
+             'revenue': ('revenue', 'float')})\
+        .finalize()
+    return query
+
+
 db=setup()
 testQuery1=query1(db)
 testQuery2=query2(db)
 testQuery3=query3(db)
+testQuery4=query4(db)
